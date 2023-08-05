bcf7a65cb3ca6f24d2e28ad212cbeb8247609d79
unknown <ahmedadel2222001@gmail.com>
2023-08-05 11:20:36 +0300

    three example in homework

---
+++ C_Programming/C_basic/Homework_1/Ex_3/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: ٠٥‏/٠٨‏/٢٠٢٣
+ *      Author: A.Adel
+ */
+#include<stdio.h>
+int main(){
+	int num1,num2;
+	printf("Enter two integer :");
+	fflush(stdout);
+	scanf("%d %d",&num1,&num2);
+	printf("sum:%d",num1+num2);
+	return 0;
+}
+
