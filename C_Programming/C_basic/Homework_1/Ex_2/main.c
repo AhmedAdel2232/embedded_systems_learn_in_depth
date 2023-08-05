aed5896f24eba9df938b6c5ff40bc9725b7a4ff2
unknown <ahmedadel2222001@gmail.com>
2023-08-05 11:06:56 +0300

    seceond example

---
+++ C_Programming/C_basic/Homework_1/Ex_2/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: ٠٥‏/٠٨‏/٢٠٢٣
+ *      Author: A.Adel
+ */
+#include<stdio.h>
+int main(){
+	int x;
+	printf("Enter an integer :");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("You Entered :%d",x);
+	return 0;
+}
+
